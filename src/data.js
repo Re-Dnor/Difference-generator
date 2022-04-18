b832249a6d4353ea4b00fa24a334537f44916e79
Re-Dnor <MokshinRE@yandex.ru>
2022-04-18 20:06:36 +0500

    fix: fixed filepath

--- src/data.js
+++ src/data.js
@@ -2,4 +2,4 @@ import fs from 'fs';
 import path from 'path';
 import process from 'process';
 
-export default (ident) => JSON.parse(fs.readFileSync(path.resolve(process.cwd(), ident), 'utf-8'));
+export default (filepath) => JSON.parse(fs.readFileSync(path.resolve(process.cwd(), filepath), 'utf-8'));
