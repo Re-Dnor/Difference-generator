00b9016d044950364edffcb4948cdbf0aa784d87
Re-Dnor <MokshinRE@yandex.ru>
2022-04-19 12:31:02 +0500

    refactor: change structure of the project

--- src/data.js
+++ src/data.js
@@ -2,4 +2,5 @@ import fs from 'fs';
 import path from 'path';
 import process from 'process';
 
+
 export default (filepath) => JSON.parse(fs.readFileSync(path.resolve(process.cwd(), filepath), 'utf-8'));
--- src/index.js
+++ src/index.js
@@ -5,17 +5,20 @@ export default function genDiff(filepath1, filepath2) {
   const file1 = dataFile(filepath1);
   const file2 = dataFile(filepath2);
 
-  const keys = Object.keys({ ...file1, ...file2 }).sort();
-  const getDeviations = keys.map((key) => {
-    if (!_.has(file2, key)) {
+  const allKeys = Object.keys({ ...file1, ...file2 }).sort();
+  const getDifferences = allKeys.map((key) => {
+    if (!_.has(file2, key)) {                //not found key in file2
       return `- ${key}: ${file1[key]}`;
-    } if (!_.has(file1, key)) {
+    }
+    if (!_.has(file1, key)) {                //not found key in file1
       return `+ ${key}: ${file2[key]}`;
-    } if (file1[key] === file2[key]) {
+    }
+    if (file1[key] === file2[key]) {         //same values
       return `  ${key}: ${file1[key]}`;
     }
-    return `- ${key}: ${file1[key]}\n+ ${key}: ${file2[key]}`;
+    return `- ${key}: ${file1[key]}\n+ ${key}: ${file2[key]}`; //different values
   });
 
-  return ['{', ...getDeviations, '}'].join('\n');
+  const result = ['{', ...getDifferences, '}'].join('\n');
+  return result;
 }
