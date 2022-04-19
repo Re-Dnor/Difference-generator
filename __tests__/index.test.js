750da236c8efc8c2f39c3da27d36f8c8ec532408
Re-Dnor <MokshinRE@yandex.ru>
2022-04-19 13:44:17 +0500

    feat: jest-test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,8 +4,7 @@ test('Shows a difference two configuration files', () => {
   const filepath1 = './__fixtures__/file1.json';
   const filepath2 = './__fixtures__/file2.json';
   const difference = genDiff(filepath1, filepath2);
-  const result =
-    `{
+  const result = `{
 - follow: false
   host: hexlet.io
 - proxy: 123.234.53.22
