f2d677ba326df099a5e78d3ebb519dd49a405fef
Re-Dnor <MokshinRE@yandex.ru>
2022-04-26 12:39:56 +0500

    refactor: boost maintainability

--- src/buildTree.js
+++ src/buildTree.js
@@ -1,27 +1,34 @@
 import _ from 'lodash';
 
+const getRemoved = (key, removedValue) => ({ type: 'removed', key, value: removedValue });
+const getAdded = (key, addedValue) => ({ type: 'added', key, value: addedValue });
+const getChanged = (key, remValue, addValue) => ({
+  type: 'changed', key, value1: remValue, value2: addValue,
+});
+const getUnchanged = (key, val) => ({ type: 'unchanged', key, value: val });
+
 export default function buildTree(file1, file2) {
   const keys = Object.keys({ ...file1, ...file2 }).sort();
 
   const getDiff = (key) => {
     if (_.isPlainObject(file1[key]) && _.isPlainObject(file2[key])) {
-      return { type: 'nested', key, children: buildTree(file1[key], file2[key]), };
+      return { type: 'nested', key, children: buildTree(file1[key], file2[key]) };
     }
 
     if (!_.has(file2, key)) {
-      return { type: 'removed', key, value: file1[key] };
+      return getRemoved(key, file1[key]);
     }
 
     if (!_.has(file1, key)) {
-      return { type: 'added', key, value: file2[key] };
+      return getAdded(key, file2[key]);
     }
 
     if (file1[key] !== file2[key]) {
-      return { type: 'changed', key, value1: file1[key], value2: file2[key] };
+      return getChanged(key, file1[key], file2[key]);
     }
 
     if (file1[key] === file2[key]) {
-      return { type: 'unchanged', key, value: file1[key], };
+      return getUnchanged(key, file1[key]);
     }
 
     return null;
